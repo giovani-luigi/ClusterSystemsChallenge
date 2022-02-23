af3c52490c56aa515f24e7bf1f5a296641722ffe
giovani-luigi <giovaniluigi@hotmail.com>
2022-02-23 19:32:50 -0300

    Add Logging types

---
+++ src/ClusterMenu/Utils/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClusterMenu.Utils {
+
+    /// <summary>
+    /// Provides an implementation of a <see cref="ILogger"/> that writes log messages to the console
+    /// </summary>
+    public class ConsoleLogger : ILogger {
+
+        /// <inheritdoc />
+        public void LogInfo(string message) {
+            Console.WriteLine($@"{DateTime.Now.ToLongTimeString()} (i) {message}");
+        }
+
+        /// <inheritdoc />
+        public void LogWarning(string message, Exception ex = null) {
+            if (ex is null) {
+                Console.WriteLine($@"{DateTime.Now.ToLongTimeString()} (!) {message}");
+            }
+            Console.WriteLine($@"{DateTime.Now.ToLongTimeString()} (!) {message}, Exception: {ex}");
+        }
+
+        /// <inheritdoc />
+        public void LogError(string message, Exception ex) {
+            Console.WriteLine($@"{DateTime.Now.ToLongTimeString()} (X) {message}, Exception: {ex}");
+        }
+    }
+}
---
+++ src/ClusterMenu/Utils/ILogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClusterMenu.Utils {
+
+    /// <summary>
+    /// Interface for loggers
+    /// </summary>
+    public interface ILogger {
+
+        /// <summary>
+        /// Write an information to the log
+        /// </summary>
+        void LogInfo(string message);
+
+        /// <summary>
+        /// Write a warning message <paramref name="message"/> to the log,
+        /// with optionally <paramref name="exception"/> as related exception.
+        /// </summary>
+        void LogWarning(string message, Exception exception = null);
+
+        /// <summary>
+        /// Write a warning message <paramref name="message"/> to the log,
+        /// with <paramref name="exception"/> as related exception.
+        /// </summary>
+        void LogError(string message, Exception exception);
+
+    }
+}
