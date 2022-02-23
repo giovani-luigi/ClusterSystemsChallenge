e85408630a51b84aeab070f15a3725255ae21fe4
giovani-luigi <giovaniluigi@hotmail.com>
2022-02-23 19:23:29 -0300

    Document custom exception class

--- src/ClusterMenu/Exceptions/DataAccessException.cs
+++ src/ClusterMenu/Exceptions/DataAccessException.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
 namespace ClusterMenu.Exceptions {
+    
+    /// <summary>
+    /// Represents an exception ocurred when accessing data
+    /// </summary>
     public class DataAccessException : ApplicationException {
 
         public DataAccessException() { }
 
         public DataAccessException(string message) : base(message) { }
+
+        public DataAccessException(string message, Exception innerException) : base(message, innerException) { }
 
     }
 }
