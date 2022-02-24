9696e32a847e06d094ad51d6f841d6b11912911c
giovani-luigi <giovaniluigi@hotmail.com>
2022-02-24 02:28:49 -0300

    Include missing files from last commit

---
+++ src/ClusterMenu/Exceptions/ModelValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClusterMenu.Exceptions {
+    
+    /// <summary>
+    /// Exception thrown when a model fails the validation
+    /// </summary>
+    public class ModelValidationException : ApplicationException {
+
+        public ModelValidationException() : base("Invalid data model") {
+        }
+
+        public ModelValidationException(string message) : base(message) {
+        }
+
+    }
+}
---
+++ src/ClusterMenu/Validators/MenuItemValidator.cs
@@ -0,0 +1,19 @@
+﻿using ClusterMenu.Model;
+using FluentValidation;
+
+namespace ClusterMenu.Validators {
+
+    /// <summary>
+    /// This is a validator for <see cref="MenuItem"/>
+    /// </summary>
+    public class MenuItemValidator : AbstractValidator<MenuItem> {
+
+        public MenuItemValidator() {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage(x => "Name is empty");
+            RuleFor(x => x.Price)
+                .GreaterThan(decimal.Zero).WithMessage(x => "Price must be greater than zero");
+        }
+
+    }
+}
