90dcb1fbf031b8a02daa73baf844a20fef522c44
giovani-luigi <giovaniluigi@hotmail.com>
2022-02-24 05:05:08 -0300

    Create tests for MainViewModel

---
+++ src/ClusterMenu.Tests/ViewModel/MainViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using ClusterMenu.Services;
+using ClusterMenu.Tests.DataAccess.Mock;
+using ClusterMenu.ViewModel;
+using NUnit.Framework;
+
+namespace ClusterMenu.Tests.ViewModel {
+    
+    [TestFixture]
+    public class MainViewModelTests {
+
+        [Test]
+        [TestCase("Cal", ExpectedResult = "Calamari")]
+        [TestCase("Corn", ExpectedResult = "Mini Corn Dogs")]
+        [TestCase("New York", ExpectedResult = "New York Strip")]
+        [TestCase("Alfredo", ExpectedResult = "Broccoli Alfredo")]
+        public string Test_Search_FirstResult_Name(string searchText) {
+
+            var vm = new MainViewModel(new MenuService(new MenuItemTestRepository()));
+
+            vm.SearchText = searchText;
+
+            var found = vm.ListItems.First();
+
+            Assert.That(found, Is.Not.Null, $"Found no object for search criteria '{searchText}'");
+
+            return found.Name;
+        }
+
+    }
+}
